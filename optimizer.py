6dac63ccaeb710055874f5ff05ead464928a58b8
jling11 <julialing11@gmail.com>
2017-03-28 10:03:45 -0700

    Filling out optimizer class with options to change elements, add q peaks, add exti, add anisotropy.

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,8 @@
 from SHELXTLFile import SHELXTLFile
 from SHELXTLDriver import SHELXTLDriver
 import os
+import numpy as np
+import shutil
 
 class Optimizer():
     def __init__(self):
@@ -9,7 +11,9 @@ class Optimizer():
 
     def run(self, path_to_SXTL_dir, ins_path, prefix):
         os.chdir(ins_path)
-        driver = SHELXTLDriver(ins_path=ins_path, prefix=prefix, path_to_SXTL_dir=path_to_SXTL_dir, is_macOS=True)
+        shutil.copy(ins_path + prefix + ".hkl", ins_path + "temp.hkl")
+        shutil.copy(ins_path + prefix + ".ins", ins_path + "temp.ins")
+        driver = SHELXTLDriver(ins_path=ins_path, prefix="temp", path_to_SXTL_dir=path_to_SXTL_dir, is_macOS=True)
 
         # Read in and run initial SHELXTL file
         ins_file = driver.get_ins_file()
@@ -17,6 +21,10 @@ class Optimizer():
         res = driver.run_SHELXTL(ins_file)
         res.write_ins()
         self.r1_history.append(res.r1)
+        self.switch_elements(driver)
+        self.try_add_q(driver)
+        self.try_anisotropy(driver)
+        self.try_exti(driver)
 
 
     def is_converged(self, count, max_iter=100):
@@ -27,23 +35,124 @@ class Optimizer():
 
     def switch_elements(self, driver):
         ins_file = driver.get_ins_file()
+
+        # Want to make changes from largest displacement to smallest
+        displacements = map((lambda x: x.displacement), ins_file.crystal_sites)
+        order = (np.argsort(np.asarray(displacements)[::-1])).tolist()
+        num_elems = len(ins_file.elements)
+        for i in order:
+            for elem in range(1, num_elems+1):
+                ins_file.change_element(i, elem)
+                self.ins_history.append(ins_file)
+                res = driver.run_SHELXTL(ins_file)
+                res.write_ins()
+                self.r1_history.append(res.r1)
+            best_elem = np.argmin(self.r1_history[-num_elems:]) + 1
+            ins_file.change_element(i, best_elem)
         self.ins_history.append(ins_file)
         res = driver.run_SHELXTL(ins_file)
         res.write_ins()
         self.r1_history.append(res.r1)
-        displacements = 
-        pass
 
-    @staticmethod
-    def try_anisotropy(ins_file):
-        pass
 
-    @staticmethod
-    def try_exti(ins_file):
-        pass
 
-    def add_q(self):
-        pass
+
+    def try_anisotropy(self, driver):
+        """
+        Test if adding anisotropy reduces R1 value.  If it does, do so.
+        :param driver: SHELXTL driver to run ins file
+        :return:
+        """
+
+        #  Try with anisotropy
+        ins_file = driver.get_ins_file()
+        ins_file.add_anisotropy()
+        self.ins_history.append(ins_file)
+        res = driver.run_SHELXTL(ins_file)
+        res.write_ins()
+        self.r1_history.append(res.r1)
+
+        #  Try without anisotropy
+        ins_file.remove_anisotropy()
+        self.ins_history.append(ins_file)
+        res = driver.run_SHELXTL(ins_file)
+        res.write_ins()
+        self.r1_history.append(res.r1)
+
+        #  If anisotropy helped, re-apply it
+        if self.r1_history[-2] < self.r1_history[-1]:
+            ins_file.add_anisotropy()
+            self.ins_history.append(ins_file)
+            res = driver.run_SHELXTL(ins_file)
+            res.write_ins()
+            self.r1_history.append(res.r1)
+
+
+
+    def try_exti(self, driver):
+        """
+        Test if adding extinguishing reduces R1 value.  If it does, do so.
+        :param driver: SHELXTL driver to run ins file
+        :return:
+        """
+
+        #  Try with extinguishing
+        ins_file = driver.get_ins_file()
+        ins_file.add_exti()
+        self.ins_history.append(ins_file)
+        res = driver.run_SHELXTL(ins_file)
+        res.write_ins()
+        self.r1_history.append(res.r1)
+
+        #  Try without exti
+        ins_file.remove_exti()
+        self.ins_history.append(ins_file)
+        res = driver.run_SHELXTL(ins_file)
+        res.write_ins()
+        self.r1_history.append(res.r1)
+
+        #  If exti helped, re-apply it
+        if self.r1_history[-2] < self.r1_history[-1]:
+            ins_file.add_exti()
+            self.ins_history.append(ins_file)
+            res = driver.run_SHELXTL(ins_file)
+            res.write_ins()
+            self.r1_history.append(res.r1)
+
+    def try_add_q(self, driver):
+        """
+        Try adding q peaks to main crystal sites if it decreases R value
+        :param driver:
+        :return:
+        """
+        r_before = self.r1_history[-1]
+        ins_file = driver.get_res_file()
+        ins_file.move_q_to_crystal()
+        num_elems = len(ins_file.elements)
+        for elem in range(1, num_elems + 1):
+            ins_file.change_element(len(ins_file.crystal_sites)-1, elem)
+            self.ins_history.append(ins_file)
+            res = driver.run_SHELXTL(ins_file)
+            res.write_ins()
+            self.r1_history.append(res.r1)
+        best_elem = np.argmin(self.r1_history[-num_elems:]) + 1
+        ins_file.change_element(len(ins_file.crystal_sites)-1, best_elem)
+        self.ins_history.append(ins_file)
+        res = driver.run_SHELXTL(ins_file)
+        res.write_ins()
+        self.r1_history.append(res.r1)
+
+        #   If adding one peak helped, recursively try adding another peak until it stops helping
+        if res.r1 < r_before:
+            self.try_add_q(driver)
+
+        #  If adding peak didn't help, take it back off
+        else:
+            ins_file.move_crystal_to_q()
+            self.ins_history.append(ins_file)
+            res = driver.run_SHELXTL(ins_file)
+            res.write_ins()
+            self.r1_history.append(res.r1)
 
     def change_occupancy(self):
         pass
