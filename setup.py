6ea5c5db418ebab0e996dd109e93daab8fed67fd
jling11 <julialing11@gmail.com>
2017-04-05 14:24:37 -0700

    Fixing setup.py to run without warnings (hopefully).

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='crystal_refinement',
     version='0.0.1',
-    packages=[''],
+    packages=['crystal_refinement'],
     url='citrine.io',
     license='BSD License',
     author='jling, eantono',
