7ad0cd23c1be7eb5fe247542ba135e214333b11f
Erin Antono <eantono@MacBook-Pro-3.local>
2017-05-22 09:33:19 -0700

    Refactor to move optimizer steps out of main optimizer class

---
+++ crystal_refinement/OptimizerUtils.py
@@ -0,0 +1,163 @@
+from pymatgen.structure_prediction.substitution_probability import SubstitutionProbability
+from pymatgen.core.composition import Element
+import re, copy
+from collections import defaultdict
+from pymatgen.io.cif import CifParser
+from SHELXDriver import SHELXDriver
+
+
+class OptimizerUtils:
+    """
+    Class for performing single crystal refinement
+    """
+
+    def __init__(self, optimizer):
+        self.optimizer = optimizer
+
+    def get_shortest_bond(self, ins_file):
+        return sorted([self.get_ideal_bond_length(el.capitalize(), el.capitalize()) for el in ins_file.elements])[0]
+
+    def get_specie(self, el, ox):
+        """
+        Return string for ion
+        :param el: Element abbreviation (eg. Fe, Au, ...)
+        :param ox: Oxidation number (eg. +2)
+        :return: eg. Fe2+
+        """
+        if ox > 0:
+            return el.symbol + str(ox) + "+"
+        else:
+            return el.symbol + str(abs(ox)) + "-"
+
+
+    def get_substitution_probability(self, el1, el2):
+        """
+        Use pymatgen to calculate substitution probability for use in site mixing
+        :param el1: name of first element
+        :param el2: name of second element
+        :return: total probability of substitution between two elements
+        """
+        sp = SubstitutionProbability()
+        total = 0
+        for o1 in el1.common_oxidation_states:
+            for o2 in el2.common_oxidation_states:
+                total += sp.prob(self.get_specie(el1, o1), self.get_specie(el2, o2))
+        return total
+
+
+    def score_compound_bonds(self, bonds, shelx_file, ml_model=None):
+        """
+        The smaller the better
+        :param bonds:
+        :param ml_model:
+        :return:
+        """
+        site_bond_scores = self.get_site_bond_scores(bonds, 1, ml_model)
+        total_stoich = 0
+        stoich_weighted_score = 0
+        for site_name, score in site_bond_scores:
+            stoich = 0
+            for site in shelx_file.crystal_sites:
+                if site.get_name().capitalize() == site_name:
+                    stoich = shelx_file.get_site_stoichiometry(site)
+            total_stoich += stoich
+            stoich_weighted_score += stoich * score
+
+        if len(site_bond_scores) < len(shelx_file.crystal_sites):
+            stoich_weighted_score -= 50
+
+        return -stoich_weighted_score / total_stoich
+
+    def get_bond_score(self, bond, ml_model=None):
+        """
+        Scores the likelihood of a bond based on its deviation from an ideal bond length.
+        :param bond: The bond to score
+        :param ml_model: Whether to use an ml model to calculate the ideal bond length.
+        TODO: The ML model needs access to the .ins file to get the chemical system
+        TODO: Might want to refactor this to use uncertainty estimates to determine the likelihood of the given bond length
+        If not, the ideal bond length is calculated as the sum of atomic radii.
+        TODO: This should be covalent radii instead
+        :return: bond length - ideal bond length
+        """
+        ideal_bond_length = self.get_ideal_bond_length(bond[0], bond[1], ml_model)
+        # Calculate amount which bonds deviate from the expected bond length
+        return -abs(bond[2] - ideal_bond_length)
+
+    def get_bond_score2(self, bond, ml_model=None):
+        """
+        Scores the likelihood of a bond based on its deviation from an ideal bond length.
+        :param bond: The bond to score
+        :param ml_model: Whether to use an ml model to calculate the ideal bond length.
+        TODO: The ML model needs access to the .ins file to get the chemical system
+        TODO: Might want to refactor this to use uncertainty estimates to determine the likelihood of the given bond length
+        If not, the ideal bond length is calculated as the sum of atomic radii.
+        TODO: This should be covalent radii instead
+        :return: bond length - ideal bond length
+        """
+        ideal_bond_length = self.get_ideal_bond_length(bond[0], bond[1], ml_model)
+        # Calculate amount which bonds deviate from the expected bond length
+        return -((bond[2] - ideal_bond_length) / 2) ** 2
+
+    def get_site_bond_scores(self, bonds, n_bonds=4, ml_model=None):
+        """
+            Determines priority for adding in site mixing.  Finds most problematic sites based on whether bonds
+            are shorter than expected.
+            TODO: What if bonds are longer than expected?
+            :param bonds: List of bond tuples (atom1, atom2, distance) for which to calculate priority
+            :return: List of (site number, site score) tuples sorted with decreasing priority.
+            """
+        bond_by_atom = defaultdict(lambda: [])
+        for bond in bonds:
+            bond_score = self.get_bond_score(bond, ml_model)
+            bond_by_atom[bond[0]].append(bond_score)
+            bond_by_atom[bond[1]].append(bond_score)
+        # Average over scores from 4 shortest bonds
+        res = map(lambda tup: (tup[0], sum(sorted(tup[1])[:n_bonds])), bond_by_atom.items())
+        # Sort by which bonds are the shortest compared to what we'd expect
+        return sorted(res, key=lambda tup: tup[1])
+
+    def site_mixing_priority(self, bonds, n_bonds=4, ml_model=None):
+
+        # Get associated site index
+        return map(lambda tup: (int(re.search("\d+", tup[0]).group(0)), tup[1]), self.get_site_bond_scores(bonds, n_bonds, ml_model))
+
+
+    def get_ideal_bond_length(self, specie_name1, specie_name2, ml_model=None):
+        """
+        Scores the likelihood of a bond based on its deviation from an ideal bond length.
+        :param specie_name1: One element in the bond
+        :param specie_name2: The other element in the bond
+        :param ml_model: Whether to use an ml model to calculate the ideal bond length.
+        TODO: The ML model needs access to the .ins file to get the chemical system
+        If not, the ideal bond length is calculated as the sum of atomic radii.
+        TODO: This should be covalent radii instead
+        :return: ideal bond length
+        """
+        if ml_model is not None:
+            pass
+
+        # Use pymatgen to get approximate bond length = sum of atomic radii
+        el1 = Element(re.sub('\d', "", specie_name1))
+        el2 = Element(re.sub('\d', "", specie_name2))
+        return el1.atomic_radius + el2.atomic_radius
+
+
+    def get_bonds(self, driver, ins_file):
+        """
+        Get the bonds defined in the given ins_file
+
+        :param ins_file: SHELXFile object
+        :return res: List of bond tuples (element 1, element 2, bond length)
+        """
+        ins_file = copy.deepcopy(ins_file)
+        ins_file.add_command("ACTA")
+        ins_file.remove_command("L.S.")
+        ins_file.add_command("L.S.", ["1"])
+        driver.run_SHELXTL(ins_file)
+
+        with open(driver.cif_file) as f:
+            cif_file = CifParser.from_string(f.read())
+
+        cif_dict = cif_file.as_dict().values()[0]
+        return zip(cif_dict["_geom_bond_atom_site_label_1"], cif_dict["_geom_bond_atom_site_label_2"],
+                   [float(x.replace("(", "").replace(")", "")) for x in cif_dict["_geom_bond_distance"]])
--- crystal_refinement/optimizer.py
+++ crystal_refinement/optimizer.py
@@ -5,6 +5,8 @@ import shutil
 from pymatgen.core.composition import Element
 import utils
 from OptimizerHistory import OptimizerHistory
+from OptimizerSteps import OptimizerSteps
+from OptimizerUtils import OptimizerUtils
 from citrination_client import CitrinationClient
 
 
@@ -30,6 +32,9 @@ class Optimizer:
         self.ml_model = None
         if use_ml_model:
             self.ml_model = CitrinationClient(os.environ["CITRINATION_API_KEY"])
+        self.utils = OptimizerUtils(self)
+        self.optimizer_steps = OptimizerSteps(self)
+
 
 
     def run(self, path_to_xl, path_to_xs, ins_path, input_prefix, output_prefix, use_wine=False, annotate_graph=False):
@@ -65,18 +70,18 @@ class Optimizer:
         self.history = OptimizerHistory(self.driver, ins_file)
 
         # Optimization
-        self.run_step(self.identify_sites)
-        self.run_step(self.switch_elements)
+        self.run_step(self.optimizer_steps.identify_sites)
+        self.run_step(self.optimizer_steps.switch_elements)
         # self.run_step(self.switch_elements)
         # There is currently an inherent assumption that switch elements will never be called after site mixing, since
         # after site mixing, the indices don't line up anymore
-        self.run_step(self.try_site_mixing)
+        self.run_step(self.optimizer_steps.try_site_mixing)
 
-        self.run_step(self.change_occupancy)
-        self.run_step(self.try_exti)
-        self.run_step(self.try_anisotropy)
-        self.run_step(self.use_suggested_weights)
-        self.run_step(self.use_suggested_weights)
+        self.run_step(self.optimizer_steps.change_occupancy)
+        self.run_step(self.optimizer_steps.try_exti)
+        self.run_step(self.optimizer_steps.try_anisotropy)
+        self.run_step(self.optimizer_steps.use_suggested_weights)
+        self.run_step(self.optimizer_steps.use_suggested_weights)
 
         self.driver.run_SHELXTL(self.history.get_best_history()[-1].ins_file)
         print "Done with optimization"
@@ -84,322 +89,6 @@ class Optimizer:
     def run_step(self, step):
         for leaf in self.history.leaves:
             step(leaf)
-
-    def get_shortest_bond(self, ins_file):
-        return sorted([utils.get_ideal_bond_length(el.capitalize(), el.capitalize()) for el in ins_file.elements])[0]
-
-    def identify_sites(self, initial):
-        if initial.r1 > self.r1_threshold:
-            self.try_add_q(initial)
-            self.identify_sites_by_bond_length(initial)
-        else:
-            self.try_add_q(initial)
-            for leaf in initial.get_leaves():
-                self.try_remove_site(leaf)
-
-    def identify_sites_by_bond_length(self, initial):
-        """
-        If the initial r1 is large, use bond lengths instead of the r1 score as the criteria for identifying which
-        electron density peaks are atom sites
-        """
-        ins_file = initial.get_res()
-        shortest_possible_bond = self.get_shortest_bond(ins_file)
-        # print shortest_possible_bond
-        prev_iteration = initial
-        for i in range(5):
-            ins_file = prev_iteration.get_res()
-            to_delete = set()
-            for bond in sorted(utils.get_bonds(self.driver, ins_file), key=lambda tup: tup[2]):
-                # print bond
-                # Threshold on how short the bonds are
-                if bond[2] / shortest_possible_bond < 0.75:
-                    a1_num = int(re.search('\d+', bond[0]).group(0))
-                    a2_num = int(re.search('\d+', bond[1]).group(0))
-                    to_delete.add(max(a1_num, a2_num))
-            # print to_delete
-            if len(to_delete) == 0:
-                break
-            ins_file.remove_sites_by_number(to_delete)
-            prev_iteration = self.history.run_and_save(ins_file, prev_iteration)
-        # quit()
-
-    def try_add_q(self, initial):
-        """
-        Try adding q peaks to main crystal sites if it decreases R value
-        :return:
-        """
-        annotation = None
-        if self.annotate_graph:
-            annotation = "Added q peak"
-        ins_file = initial.get_res()
-        # This threshold could be scaled based on the potential atoms
-        lightest_element = min([Element(el.capitalize()).number for el in ins_file.elements])
-        if ins_file.q_peaks[0].electron_density > lightest_element:
-            ins_file.move_q_to_crystal()
-            # Find best element for new site
-            num_elems = len(ins_file.elements)
-            iterations = []
-            for elem in range(1, num_elems + 1):
-                ins_file.change_element(len(ins_file.crystal_sites) - 1, elem)
-                iteration = self.history.run_iter(ins_file, initial, annotation)
-                if iteration is not None:
-                    iterations.append(iteration)
-            iterations.sort(key=lambda i: i.r1)
-            best_iter = iterations[0]
-            displacements = [x.displacement for x in best_iter.res_file.crystal_sites]
-            if best_iter.r1 < initial.r1: #and displacements[-1] < (np.mean(displacements[:-1]) + 2.0 * np.std(displacements[:-1])):
-                self.history.save([iterations[0]])
-                for leaf in initial.get_leaves():
-                    #   If adding one peak helped, recursively try adding another peak until it stops helping
-                    self.try_add_q(leaf)
-
-    def try_remove_site(self, initial):
-        """
-        Remove crystal sites if they result in bond distances that are too short
-        :param ml_model: if True, a machine learning model is used to predict the correct bond length.
-                         if False, the ideal bond length is approximated as the sum of the atomic radii
-        :return:
-        """
-
-        ins_file = initial.get_res()
-        r_penalty = 1.1
-        bonds = utils.get_bonds(self.driver, ins_file)
-        threshold = 0.1
-        # print len(ins_file.crystal_sites)
-        while True:
-            ins_file = initial.get_res()
-            to_delete = set()
-            for a1, a2, distance in bonds:
-                ideal_distance = utils.get_ideal_bond_length(a1, a2, self.ml_model)
-                # if the distance is too small, remove the lower density atom
-                if (ideal_distance - distance) / ideal_distance > threshold:
-                    a1_num = int(re.search('\d+', a1).group(0))
-                    a2_num = int(re.search('\d+', a2).group(0))
-                    to_delete.add(max(a1_num, a2_num))
-            # no sites removed
-            if len(to_delete) == 0:
-                break
-            ins_file.remove_sites_by_number(to_delete)
-            ins_file.renumber_sites()
-            if self.annotate_graph:
-                cur_iter = self.history.run_iter(ins_file, initial, "Removed {} site(s)".format(len(to_delete)))
-            else:
-                cur_iter = self.history.run_iter(ins_file, initial)
-            if cur_iter is not None and cur_iter.r1 < initial.r1 * r_penalty:
-                self.history.save(cur_iter)
-                break
-            threshold *= 1.1
-        # quit()
-
-    def switch_elements(self, initial):
-        ins_file = initial.get_res()
-
-        # Want to make changes from largest displacement to smallest
-        sorted_sites = sorted(ins_file.crystal_sites, key=lambda s: -s.displacement)
-        order = [s.site_number - 1 for s in sorted_sites]
-        num_elems = len(ins_file.elements)
-
-        # print ins_file.get_crystal_sites_text()
-        for i in order:
-            for prev_iter in initial.get_leaves():
-                ins_file = prev_iter.get_res()
-                iterations = []
-                for elem in range(1, num_elems+1):
-                    ins_file.change_element(i, elem)
-                    if self.annotate_graph:
-                        prev = prev_iter.res_file.crystal_sites[i].get_name()
-                        cur = ins_file.crystal_sites[i].get_name()
-                        iteration = self.history.run_iter(ins_file, prev_iter, "Changed {} to {}".format(prev, cur))
-                    else:
-                        iteration = self.history.run_iter(ins_file, prev_iter)
-                    if iteration is not None:
-                        iterations.append(iteration)
-                iterations.sort(key=lambda i: i.r1)
-                # print prev_iter.res_file.crystal_sites[i].get_name()
-                for iteration in iterations:
-                    # print iteration.ins_file.crystal_sites[i].get_name(), iteration.r1, iteration.bond_score, iteration.overall_score
-                    if iteration.r1 - iterations[0].r1 < self.r1_similarity_threshold:
-                        # print "saved"
-                        self.history.save(iteration)
-            self.history.clean_history()
-                # print "#"*50
-        # quit()
-
-    def change_occupancy(self, initial):
-        ins_file = initial.get_res()
-
-        # Want to make changes from largest displacement to smallest
-        displacements = map((lambda x: x.displacement), ins_file.crystal_sites)
-
-        for i, displacement in sorted(enumerate(displacements), key=lambda tup: -tup[1]):
-            # don't change occupancy of mixed sites
-            if ins_file.crystal_sites[i].site_number in ins_file.mixed_site_numbers:
-                continue
-            for prev_iter in initial.get_leaves():
-                ins_file = prev_iter.get_res()
-
-                # only change occupancy if displacement is >= 2 std deviations away from mean
-                mean = np.mean(displacements[:i] + displacements[i+1:])
-                std = np.std(displacements[:i] + displacements[i+1:])
-                if abs(displacement - mean) / std < 2.0:
-                    break
-
-                ins_file.add_variable_occupancy(i)
-                if self.annotate_graph:
-                    site = ins_file.crystal_sites[i].get_name()
-                    iteration = self.history.run_iter(ins_file, initial, "Added variable occupancy for {}".format(site))
-                else:
-                    iteration = self.history.run_iter(ins_file, initial)
-
-                # If changing the occupancy decreased r1, decreased the displacement, and resulted in an occupancy
-                # that satisfies the threshold, add it to the history
-                if iteration is not None \
-                        and iteration.r1 < prev_iter.r1 \
-                        and float(iteration.res_file.fvar_vals[-1]) < (1 - self.occupancy_threshold) \
-                        and iteration.res_file.crystal_sites[i].displacement < displacement:
-                    self.history.save(iteration)
-
-    def try_site_mixing(self, initial):
-        """
-        Try allowing site mixing, where a single crystal site might have mixed occupancy between two different elements
-        Only handles pair-wise mixing.
-        :param driver: SHELX driver
-        """
-        ins_file = initial.get_res()
-        element_list = [Element(el.capitalize()) for el in ins_file.elements]
-        pairs = []
-        probability_threshold = 2E-4
-
-        # For all elements in compound, calculate substitution probabilities
-        # If substitution probability is > probability_threshold, then save it to pairs list.
-        for i1, i2 in itertools.combinations(range(len(element_list)), 2):
-            e1 = element_list[i1]
-            e2 = element_list[i2]
-            sp = utils.get_substitution_probability(e1, e2)
-            # print e1, e2, sp
-            if sp > probability_threshold:
-                pairs.append(([i1, i2], sp))
-
-        # Sort pairs by substitution probability (largest to smallest)
-        pairs = [tup[0] for tup in sorted(pairs, key=lambda tup: -tup[1])]
-
-        # Keep adding site mixing until we've already tried adding site mixing at the top priority sites
-        tried = set()
-        self.do_site_mixing(initial, tried, pairs)
-        # quit()
-
-    def do_site_mixing(self, initial, tried, pairs):
-        ins_file = initial.get_res()
-        bonds = utils.get_bonds(self.driver, ins_file)
-        mixing_priority = utils.site_mixing_priority(bonds)
-        # In case of ties, find all top tied priorities
-        top_priority_score = mixing_priority[0][1]
-        top_priority = [priority[0] for priority in mixing_priority if priority[1] - top_priority_score < 0.5]
-        # For each of these top priorities, try site mixing
-        if all([i in tried for i in top_priority]):
-            return
-        for i in top_priority:
-            if i in tried:
-                continue
-            for prev_iter in initial.get_leaves():
-                tried.add(i)
-                iterations = []
-                for pair in pairs:
-                    ins_file = prev_iter.get_res()
-                    ins_file.add_site_mixing(site_number=i, mixing_element_indices=pair)
-                    if self.annotate_graph:
-                        mix = "{} and {}".format(ins_file.elements[pair[0]], ins_file.elements[pair[1]])
-                        iteration = self.history.run_iter(ins_file, prev_iter, "Mixing {} on site {}".format(mix, i))
-                    else:
-                        iteration = self.history.run_iter(ins_file, prev_iter)
-                    if iteration is not None:
-                        occupancy_var = float(iteration.res_file.fvar_vals[-1])
-                        # Only include occupancies that are actually split
-                        if occupancy_var > self.occupancy_threshold and occupancy_var < (1 - self.occupancy_threshold):
-                            iterations.append(iteration)
-
-                if len(iterations) == 0:
-                    continue
-                iterations.sort(key=lambda i: i.r1)
-                best_r1 = min([prev_iter.r1, iterations[0].r1])
-                if prev_iter.r1 - best_r1 < self.r1_similarity_threshold:
-                    prev_iter.propagate()
-                if iterations[0].r1 - best_r1 < self.r1_similarity_threshold:
-                    self.history.save(iterations[0])
-        self.history.clean_history()
-        for leaf in initial.get_leaves():
-            self.do_site_mixing(leaf, tried.union(set(top_priority)), pairs)
-
-
-    def try_anisotropy(self, initial):
-        """
-        Test if adding anisotropy reduces R1 value.  If it does, do so.
-        :return:
-        """
-
-        ins_file = initial.get_res()
-
-        #  Try with anisotropy
-        ins_file.add_anisotropy()
-        if self.annotate_graph:
-            iteration = self.history.run_iter(ins_file, initial, "Added anisotropy")
-        else:
-            iteration = self.history.run_iter(ins_file, initial)
-        #  If anisotropy helped, add it to the history
-        # if iteration is not None and iteration.r1 < initial.r1:
-        #     self.history.save(iteration)
-
-        if iteration is not None:
-            self.history.save(iteration)
-            if iteration.r1 > initial.r1:
-                initial.propagate()
-
-    def try_exti(self, initial):
-        """
-        Test if adding extinguishing reduces R1 value.  If it does, do so.
-        :return:
-        """
-        ins_file = initial.get_res()
-
-        #  Try with extinguishing
-        ins_file.add_exti()
-        if self.annotate_graph:
-            iteration = self.history.run_iter(ins_file, initial, "Added extinction")
-        else:
-            iteration = self.history.run_iter(ins_file, initial)
-
-        #  If exti helped, add it to the history
-        # if iteration is not None and iteration.r1 < initial.r1:
-        #     self.history.save(iteration)
-        if iteration is not None:
-            self.history.save(iteration)
-            if iteration.r1 > initial.r1:
-                initial.propagate()
-
-    def use_suggested_weights(self, initial):
-        """
-        Stop re-initializing weights each time--use previously suggested weights
-        :return:
-        """
-        ins_file = initial.get_res()
-
-        #  Try with extinguishing
-        ins_file.remove_command("WGHT")
-        ins_file.commands.append(("WGHT", ins_file.suggested_weight_vals))
-        if self.annotate_graph:
-            iteration = self.history.run_iter(ins_file, initial, "Used suggested weights")
-        else:
-            iteration = self.history.run_iter(ins_file, initial)
-
-        #  If weights helped, add it to the history
-        # if iteration is not None and iteration.r1 < initial.r1:
-        #     self.history.save(iteration)
-
-        if iteration is not None:
-            self.history.save(iteration)
-            if iteration.r1 > initial.r1:
-                initial.propagate()
-
 
 ########################################################################################################################
 
