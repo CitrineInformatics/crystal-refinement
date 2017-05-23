86263506cdad0624d602e582493654986e61aa32
jling11 <julialing11@gmail.com>
2017-05-23 16:01:32 -0700

    Moved mains and tests to separate files.

--- crystal_refinement/SHELXFile.py
+++ crystal_refinement/SHELXFile.py
@@ -293,17 +293,3 @@ class SHELXFile:
         """
         return [i for i, site in enumerate(self.crystal_sites) if site.site_number == number]
 
-def main():
-    test_file = "/Users/julialing/Documents/DataScience/crystal_refinement/4-2-1-4_single_crystal/Example_from_slides/7.ins"
-
-    with open(test_file) as f:
-        text = f.read()
-        print text
-        print "\n\n" + "~"*50 + "\n\n"
-        file_obj = SHELXFile(text)
-
-        print file_obj.write_ins()
-
-
-if __name__ == "__main__":
-    main()
