a381705cae517d8719dc0703e1a3421fb52ffe30
Erin Antono <erin@citrine.io>
2018-05-30 09:13:02 -0700

    added installation helpers

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='crystal_refinement',
     version='0.1.0',
-    packages=['crystal_refinement'],
+    packages=find_packages(),
     install_requires=['numpy',
                       'pymatgen',
-                      'citrination-client',
+                      'citrination-client>=4.0.0',
                       'graphviz'],
     url='citrine.io',
     license='BSD License',
     author='jling, eantono',
     author_email='erin (at) citrine (dot) io',
-    description='Helps automate shelx for single crystal refinement'
+    description='Helps automate shelx for single crystal refinement',
+    entry_points={
+            'console_scripts': [
+                  'crystal_refinement=crystal_refinement.__main__:main'
+            ]
+    }
 )
